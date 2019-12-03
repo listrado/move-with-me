3b04217a778d6d598b0389cd9e365068f862824e
arigbede emmanuel <Harriemannie@gmail.com>
2019-12-03 18:03:23 -0300

    seed database

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: %i[show] do
-  	resources :journey, only: %i[new create]
-  end
+  resources :users, only: %i[show]
+  resources :journeys, only: %i[new create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Group.destroy_all
+Journey.destroy_all
+Location.destroy_all
+
+
+
+puts "creating locations"
+first_location = Location.create!(
+  address: '16 Villa Gaudelet, Paris',
+  latitude: 48.8648482,
+  longitude: 2.3798534
+)
+
+second_location = Location.create!(
+  address: 'The Hôtel de Ville, Paris, France',
+  latitude: 48.856373,
+  longitude: 2.353016
+)
+
+
+puts "creating users"
+
+first_user = User.create!(
+  email: 'user1@gmail.com',
+  password: '123456'
+  )
+
+
+second_user = User.create!(
+  email: 'user2@gmail.com',
+  password: '123456'
+  )
+
+
+puts "creating groups"
+# Group going from 1 to 2
+populated_group = Group.create!(
+  start_at: Time.now,
+  start_location: first_location,
+  end_location: second_location
+)
+
+# Group going from 2 to 1
+empty_group = Group.create!(
+  start_at: Time.now,
+  start_location: second_location,
+  end_location: first_location
+)
+
+
+puts "creating journeys"
+# Journeys: Users 1 and 2 join Group 1
+first_journey = Journey.create!(
+  group: populated_group,
+  user: first_user
+)
+second_journey = Journey.create!(
+  group: populated_group,
+  user: second_user
+)
+
+# Journeys: Noboby joins group 2
+
+
+puts "seed completed"
+
