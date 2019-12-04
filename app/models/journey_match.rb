dd6218c9447ee143ec2748cd169994dcb13a4c19
arigbede emmanuel <Harriemannie@gmail.com>
2019-12-04 15:22:26 -0300

    user can create journey directly from the dashboard

--- app/models/journey_match.rb
+++ app/models/journey_match.rb
@@ -1,7 +1,7 @@
 class JourneyMatch
   include ActiveModel::Model
 
-  attr_accessor :start_hour, :start_minute, :destination_address
+  attr_accessor :start_hour, :start_minute, :departure_address, :destination_address
 
   def start_at
     DateTime.parse(Date.today.to_s + " #{start_hour}:#{start_minute}")
