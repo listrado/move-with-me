a2c6b24bea87ef5261b4025f0f23c802a96dc877
arigbede emmanuel <Harriemannie@gmail.com>
2019-12-04 17:01:13 -0300

    revert-journey-matches-form

--- app/controllers/journey_matches_controller.rb
+++ app/controllers/journey_matches_controller.rb
@@ -4,21 +4,18 @@ class JourneyMatchesController < ApplicationController
   end
 
   def create
-    # params = journey_params
+    # params = journey_matches_params
 
-    @match = JourneyMatch.new(journey_params)
-    @group = Group.where(start_at: @match.start_at)
+    @journey_match = JourneyMatch.new(journey_matches_params)
+    @group = Group.where(start_at: @journey_match.start_at)
     # start -> Location.near([45,45], 0.3)
     # end -> Location.near('16 Villa Gaudelet, Paris', 0.3)
-    raise
     redirect_to user_path(current_user)
   end
 
   private
 
-  def journey_params
-    binding.pry
-
+  def journey_matches_params
     # params[:start_time] = "#{params['journey_match']['start_time(4i)']}:#{params['journey_match']['start_time(5i)']}"
     params.require(:journey_match).permit(:start_hour, :start_minute, :departure_address, :destination_address)
   end
