81ea956770301093b343892423cc52bf1affc690
lucas cardi <llucas.cardi.rangel@gmail.com>
2019-12-05 12:56:00 -0300

    user gets auto added to group were start time start location and end location are the same "add end location"

--- app/controllers/journey_matches_controller.rb
+++ app/controllers/journey_matches_controller.rb
@@ -4,10 +4,6 @@ class JourneyMatchesController < ApplicationController
   end
 
   def create
-    # params = journey_matches_params
-
-    # binding.pry
-
     @journey_match = JourneyMatch.new(journey_matches_params)
 
     # location_params = {
@@ -19,16 +15,9 @@ class JourneyMatchesController < ApplicationController
     groups = Group.where(start_at: @journey_match.start_at)
     location_start = Location.find_or_create_by(address: @journey_match.departure_address)
     location_end = Location.find_or_create_by(address: @journey_match.destination_address)
-    groups.each do |group|
-      Location.near([location_start.latitude, location_start.longitude], 0.3).each do |location|
-        next unless location == group.start_location
-        @group = group
-        journey = Journey.new(group: @group, user: current_user)
-        journey.save
-        break
-      end
-      break if @group
-    end
+
+    group_find(groups, location_start, location_end)
+
     unless @group
       @group = Group.new(
         start_at: @journey_match.start_at,
@@ -36,13 +25,29 @@ class JourneyMatchesController < ApplicationController
         end_location: location_end
       )
       @group.save
-      journey = Journey.new(group: @group, user: current_user)
-      journey.save
     end
+
+    journey = Journey.new(group: @group, user: current_user)
+    journey.save
     redirect_to user_path(current_user)
   end
 
   private
+
+  def group_find(groups, location_start, location_end)
+    groups.each do |group|
+      Location.near([location_start.latitude, location_start.longitude], 0.3).each do |start_location|
+        next unless start_location == group.start_location
+        Location.near([location_end.latitude, location_end.longitude], 0.3).each do |end_location|
+          next unless end_location == group.end_location
+          @group = group
+          break
+        end
+        break
+      end
+      break if @group
+    end
+  end
 
   def journey_matches_params
     # params[:start_time] = "#{params['journey_match']['start_time(4i)']}:#{params['journey_match']['start_time(5i)']}"
