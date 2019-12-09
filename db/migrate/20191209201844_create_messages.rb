f32972f009a96b4bea28359f636a0b4e364c3659
lucas cardi <llucas.cardi.rangel@gmail.com>
2019-12-09 17:23:01 -0300

    mesage

--- db/migrate/20191209201844_create_messages.rb
+++ db/migrate/20191209201844_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       t.string :subject
       t.references :user, foreign_key: true
+      t.references :group, foreign_key: true
 
       t.timestamps
     end
