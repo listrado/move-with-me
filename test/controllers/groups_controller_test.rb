65b5bcaa8da59f341b988c9c51059db2a93765de
lucas cardi <llucas.cardi.rangel@gmail.com>
2019-12-09 15:05:19 -0300

    tester update

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class GroupsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get groups_index_url
+  test "should get show" do
+    get groups_show_url
     assert_response :success
   end
 
