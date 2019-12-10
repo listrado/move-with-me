d526223e2e340c48e5024bfef8b6d49aedb1ef53
arigbede emmanuel <Harriemannie@gmail.com>
2019-12-10 17:59:16 -0300

    create the journey detail and chat view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,17 @@
 class MessagesController < ApplicationController
+  def index
+    @group = Group.find(params[:group_id])
+    @message = Message.new
+    @messages = Message.all
+  end
+
   def create
     @message = Message.new(message_params)
     @message.group_id = params[:group_id]
     @message.user = current_user
 
     if @message.save
-      redirect_to user_group_path(current_user, @message.group)
+      redirect_to user_group_messages_path(current_user, current_user.journeys.last.group)
     end
   end
 
