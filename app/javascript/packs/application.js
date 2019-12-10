d503881b83a062648ad220a597dd0262de63f301
Artur Pedrosa <maverickanp@gmail.com>
2019-12-10 18:39:40 -0300

    fixed the js from journeymatches

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,14 @@
 import "bootstrap";
 import "jquery";
 
-import "mapbox-gl/dist/mapbox-gl.css"; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
-import { initMapbox } from "../plugins/init_mapbox";
-import { initJourneyMatchesForm } from "../components/journey_matches_form";
+import {
+    initMapbox
+} from '../plugins/init_mapbox';
+import {
+    initJourneyMatchesForm
+} from '../components/journey_matches_form'
 
 initMapbox();
-// initJourneyMatchesForm();
+initJourneyMatchesForm();
